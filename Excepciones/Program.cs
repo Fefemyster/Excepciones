e5983bb32c6bb552b41c97b0d32c3c1253c19bb3
Fefemyster <kenennlara@gmail.com>
2025-08-19 15:12:38 -0600

    Add project files.

---
+++ Excepciones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+bool detener = false;
+
+do
+{
+    Console.WriteLine("Presione 0 para salir");
+    Console.WriteLine("Ingrese la cantidad de notas a evaluar");
+
+    try
+    {
+        int cantidadNotas = Convert.ToInt32(Console.ReadLine());
+        if (cantidadNotas == 0)
+        {
+            detener = true;
+        }
+    }
+    catch (FormatException e) { Console.WriteLine("Valor invalido"); }
+    
+
+}
+while (!detener);
+
+
+
+
+
+
+Console.WriteLine("Ingrese sus notas");
+int notas = Convert.ToInt32(Console.ReadLine());
+
